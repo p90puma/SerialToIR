920b24b75623a57882f4126db6bdbd56e41375af
p90puma <puma1337@gmail.com>
2012-08-12 23:50:11 -0400

    added WSDL calls via CXF.

---
+++ SerialToIRMobile/src/main/java/com/p90puma/serialtoirmobile/impl/SerialToIRImpl.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.p90puma.serialtoirmobile.impl;
+
+import com.p90puma.arduinows.ArduinoWS;
+import org.apache.cxf.interceptor.LoggingInInterceptor;
+import org.apache.cxf.interceptor.LoggingOutInterceptor;
+import org.apache.cxf.jaxws.JaxWsProxyFactoryBean;
+
+/**
+ *
+ * @author sjakubowski
+ */
+public class SerialToIRImpl {   
+    
+    public void sendSerialInput(String msg){       
+        
+        JaxWsProxyFactoryBean factory = new JaxWsProxyFactoryBean();
+        factory.getInInterceptors().add(new LoggingInInterceptor());
+        factory.getOutInterceptors().add(new LoggingOutInterceptor());
+        factory.setServiceClass(ArduinoWS.class);
+        factory.setAddress("http://localhost:8084/ArduinoWS/ArduinoWSService");
+        ArduinoWS client = (ArduinoWS) factory.create();
+        String reply = client.sendSerialInput(msg);
+        System.out.println("Server said: " + reply);
+    }
+    
+}
