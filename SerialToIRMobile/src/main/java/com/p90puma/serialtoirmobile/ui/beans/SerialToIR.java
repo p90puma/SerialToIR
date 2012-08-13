4c0c82c7789660ea14c1affdab649a87f509692b
p90puma <puma1337@gmail.com>
2012-08-12 22:27:39 -0400

    Added simple empty mobile UI.

---
+++ SerialToIRMobile/src/main/java/com/p90puma/serialtoirmobile/ui/beans/SerialToIR.java
@@ -0,0 +1,40 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.p90puma.serialtoirmobile.ui.beans;
+
+
+import javax.faces.bean.ApplicationScoped;
+import javax.faces.bean.ManagedBean;
+
+/**
+ *
+ * @author sjakubowski
+ */
+
+@ManagedBean
+@ApplicationScoped
+public class SerialToIR
+{
+    public void sendPower(){
+        
+    }
+    
+    public void sendOptical(){
+        
+    }
+    
+    public void sendDirect(){
+        
+    }
+    
+    public void sendVolUP(){
+        
+    }
+    
+    public void sendVolDOWN(){
+        
+    }
+    
+}
