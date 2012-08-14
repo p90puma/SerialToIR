6342b2ac819cf7858537517b09ba97534bf90d3b
p90puma <puma1337@gmail.com>
2012-08-13 23:10:13 -0400

    fixed z5500 codes, minor structure refactoring.

---
+++ SerialToIRMobile/src/main/java/com/p90puma/serialtoirmobile/data/Z5500.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.p90puma.serialtoirmobile.data;
+
+/**
+ *
+ * @author sjakubowski
+ */
+public class Z5500 {
+    public final static String POWER = "0x10EF08F7";
+    public final static String DIRECT = "0x10EF50AF";
+    public final static String OPTICAL = "0x10EFD02F";
+    public final static String VOLUP = "0x10EF58A7";
+    public final static String VOLDOWN = "0x10EF708F";
+}
--- SerialToIRMobile/src/main/java/com/p90puma/serialtoirmobile/ui/beans/SerialToIR.java
+++ SerialToIRMobile/src/main/java/com/p90puma/serialtoirmobile/ui/beans/SerialToIR.java
@@ -4,6 +4,7 @@
  */
 package com.p90puma.serialtoirmobile.ui.beans;
 
+import com.p90puma.serialtoirmobile.data.Z5500;
 import com.p90puma.serialtoirmobile.impl.SerialToIRImpl;
 import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
@@ -16,31 +17,25 @@ import javax.faces.bean.ManagedBean;
 @ApplicationScoped
 public class SerialToIR {
 
-    private final static String Z5500_POWER = "10EF08F7";
-    private final static String Z5500_DIRECT = "10EF08F7";
-    private final static String Z5500_OPTICAL = "10EF08F7";
-    private final static String Z5500_VOLUP = "10EF08F7";
-    private final static String Z5500_VOLDOWN = "10EF08F7";
-    
     SerialToIRImpl action = new SerialToIRImpl();
 
     public void sendPower() {
-        action.sendSerialInput(Z5500_POWER);
+        action.sendSerialInput(Z5500.POWER);
     }
 
     public void sendOptical() {
-        action.sendSerialInput(Z5500_OPTICAL);
+        action.sendSerialInput(Z5500.OPTICAL);
     }
 
     public void sendDirect() {
-        action.sendSerialInput(Z5500_DIRECT);
+        action.sendSerialInput(Z5500.DIRECT);
     }
 
     public void sendVolUP() {
-        action.sendSerialInput(Z5500_VOLUP);        
+        action.sendSerialInput(Z5500.VOLUP);        
     }
 
     public void sendVolDOWN() {
-        action.sendSerialInput(Z5500_VOLDOWN);
+        action.sendSerialInput(Z5500.VOLDOWN);
     }
 }
