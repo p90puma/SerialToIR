064989664a443892be3f2b993ead4d43ab8f4d30
p90puma <puma1337@gmail.com>
2012-08-15 22:06:56 -0400

    added more devices to mobile UI.

---
+++ SerialToIRMobile/src/main/java/com/p90puma/serialtoirmobile/data/HD20.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.p90puma.serialtoirmobile.data;
+
+/**
+ *
+ * @author sjakubowski
+ */
+public class HD20 {
+    public final static String POWERON = "0x4CB340BF";
+    public final static String POWEROFF = "0x4CB310EF";
+}
---
+++ SerialToIRMobile/src/main/java/com/p90puma/serialtoirmobile/data/HDMISwitch.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.p90puma.serialtoirmobile.data;
+
+/**
+ *
+ * @author sjakubowski
+ */
+public class HDMISwitch {
+    public final static String INPUT1 = "0xFF10EF";
+    public final static String INPUT2 = "0xFF50AF";
+    public final static String INPUT3 = "0xFF30CF";
+    public final static String INPUT4 = "0xFF708F";
+
+}
