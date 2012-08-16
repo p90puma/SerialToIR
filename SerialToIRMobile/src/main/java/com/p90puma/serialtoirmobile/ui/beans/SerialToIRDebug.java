64e82f5422e9876d3ddfd8849646aad0666df690
p90puma <puma1337@gmail.com>
2012-08-15 21:36:33 -0400

    updated to latest version of wsdl in mobile project. added exception to output of getSerialPorts in WSDL. added getSerialPorts to debug screen in Mobile app.

--- SerialToIRMobile/src/main/java/com/p90puma/serialtoirmobile/ui/beans/SerialToIRDebug.java
+++ SerialToIRMobile/src/main/java/com/p90puma/serialtoirmobile/ui/beans/SerialToIRDebug.java
@@ -5,6 +5,7 @@
 package com.p90puma.serialtoirmobile.ui.beans;
 
 import com.p90puma.serialtoirmobile.impl.SerialToIRImpl;
+import java.util.ArrayList;
 import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
 
@@ -19,6 +20,7 @@ public class SerialToIRDebug {
     SerialToIRImpl action = new SerialToIRImpl();
 
     private String port = "ttyUSB0"; //default
+    private String ports = "Click me->"; //default
 
     public String getPort() {
         return port;
@@ -28,8 +30,25 @@ public class SerialToIRDebug {
         this.port = port;
     }
 
+    public String getPorts() {
+        return ports;
+    }
+
+    public void setPorts(String ports) {
+        this.ports = ports;
+    }
+
     public void setupSerial() {
         action.setupSerial(port);
     }
 
+    public void getSerialPorts() {
+        ArrayList<String> serialPorts = action.getSerialPorts();
+        ports = "";
+        for (String str : serialPorts) {
+            ports = ports + str + " ";
+        }
+        ports = ports.trim();
+        setPorts(ports);
+    }
 }
